40a6d59c0ed1bbd96e0fdae2336376a99dd30b5b
Muhammad Ghazy <muhammadghazismp85@gmail.com>
2026-07-29 21:04:33 +0700

    modifying if customer only buy food only or drink only

--- receipt.py
+++ receipt.py
@@ -7,32 +7,43 @@ def total_price_generator(cart, price_data):
     subtotal = sum(price_data[item]['price'] for item in cart)
     return subtotal, subtotal*0.12
 
-def print_cart(cart, price_data):
+def print_cart(cart, price_data, transaction_id):
+
+    food_check = any(price_data[item]['type'] == 'food' for item in cart)
+    drink_check = any(price_data[item]['type'] == 'drink' for item in cart)
+
+    print(f"Transaction ID: {transaction_id}")
     print("=============================== Your Order ===============================")
 
-    print('Foods\n--------------------------------------------------------------------------')
-    food_item = {}
-    for item in cart:
-        if price_data[item]['type'] == 'food':
-            if item not in food_item.keys():
-                food_item[item] = 1
-            elif item in food_item.keys():
-                food_item[item] += 1
-    for item, quantity in food_item.items():
-        item_data = price_data[item]
-        print(f"{quantity} x {item_data['name']:<46} Rp {item_data['price'] * quantity:,}")
+    if food_check is True:
+        print('Foods\n--------------------------------------------------------------------------')
+        food_item = {}
+        for item in cart:
+            if price_data[item]['type'] == 'food':
+                if item not in food_item.keys():
+                    food_item[item] = 1
+                elif item in food_item.keys():
+                    food_item[item] += 1
+        for item, quantity in food_item.items():
+            item_data = price_data[item]
+            print(f"{quantity} x {item_data['name']:<46} Rp {item_data['price'] * quantity:,}")
+    else:
+        pass
 
-    print('\nDrinks\n--------------------------------------------------------------------------')
-    drink_item = {}
-    for item in cart:
-        if price_data[item]['type'] == 'drink':
-            if item not in drink_item.keys():
-                drink_item[item] = 1
-            elif item in drink_item.keys():
-                drink_item[item] += 1
-    for item, quantity in drink_item.items():
-        item_data = price_data[item]
-        print(f"{quantity} x {item_data['name']:<46} Rp {item_data['price'] * quantity:,}")
+    if drink_check is True:
+        print('\nDrinks\n--------------------------------------------------------------------------')
+        drink_item = {}
+        for item in cart:
+            if price_data[item]['type'] == 'drink':
+                if item not in drink_item.keys():
+                    drink_item[item] = 1
+                elif item in drink_item.keys():
+                    drink_item[item] += 1
+        for item, quantity in drink_item.items():
+            item_data = price_data[item]
+            print(f"{quantity} x {item_data['name']:<46} Rp {item_data['price'] * quantity:,}")
+    else:
+        pass
 
     subtotal, vat_service = total_price_generator(cart, price_data)
 
@@ -43,32 +54,42 @@ def print_cart(cart, price_data):
     print("==========================================================================")
 
 
-def print_cart_after_payment(cart, price_data, pay_amount, change):
+def print_cart_after_payment(cart, price_data, pay_amount, change, transaction_id):
+
+    food_check = any(price_data[item]['type'] == 'food' for item in cart)
+    drink_check = any(price_data[item]['type'] == 'drink' for item in cart)
+
+    print(f"Transaction ID: {transaction_id}")
     print("=============================== Your Order ===============================")
+    if food_check is True:
+        print('Foods\n--------------------------------------------------------------------------')
+        food_item = {}
+        for item in cart:
+            if price_data[item]['type'] == 'food':
+                if item not in food_item.keys():
+                    food_item[item] = 1
+                elif item in food_item.keys():
+                    food_item[item] += 1
+        for item, quantity in food_item.items():
+            item_data = price_data[item]
+            print(f"{quantity} x {item_data['name']:<46} Rp {item_data['price'] * quantity:,}")
+    else:
+        pass
 
-    print('Foods\n--------------------------------------------------------------------------')
-    food_item = {}
-    for item in cart:
-        if price_data[item]['type'] == 'food':
-            if item not in food_item.keys():
-                food_item[item] = 1
-            elif item in food_item.keys():
-                food_item[item] += 1
-    for item, quantity in food_item.items():
-        item_data = price_data[item]
-        print(f"{quantity} x {item_data['name']:<46} Rp {item_data['price'] * quantity:,}")
-
-    print('\nDrinks\n--------------------------------------------------------------------------')
-    drink_item = {}
-    for item in cart:
-        if price_data[item]['type'] == 'drink':
-            if item not in drink_item.keys():
-                drink_item[item] = 1
-            elif item in drink_item.keys():
-                drink_item[item] += 1
-    for item, quantity in drink_item.items():
-        item_data = price_data[item]
-        print(f"{quantity} x {item_data['name']:<46} Rp {item_data['price'] * quantity:,}")
+    if drink_check is True:
+        print('\nDrinks\n--------------------------------------------------------------------------')
+        drink_item = {}
+        for item in cart:
+            if price_data[item]['type'] == 'drink':
+                if item not in drink_item.keys():
+                    drink_item[item] = 1
+                elif item in drink_item.keys():
+                    drink_item[item] += 1
+        for item, quantity in drink_item.items():
+            item_data = price_data[item]
+            print(f"{quantity} x {item_data['name']:<46} Rp {item_data['price'] * quantity:,}")
+    else:
+        pass
 
     subtotal, vat_service = total_price_generator(cart, price_data)
 
